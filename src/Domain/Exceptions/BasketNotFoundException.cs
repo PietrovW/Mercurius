ab6ba263600980020c39fc5de3016e38ed85c9ff
Piotr Walacik <piotr.walacik@icloud.com>
2024-03-10 17:43:43 +0100

    Adding your own exceptions #8

---
+++ src/Domain/Exceptions/BasketNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Exceptions;
+
+public sealed class ExceptionInfoNotFoundException : Exception
+{
+    public ExceptionInfoNotFoundException(Guid exceptionInfoId) : base($"No ExceptionInfo found with id {exceptionInfoId}")
+    {
+    }
+}
---
+++ src/Domain/Exceptions/DuplicateException.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Exceptions;
+
+public sealed class DuplicateException : Exception
+{
+    public DuplicateException(string message) : base(message)
+    {
+    }
+}
