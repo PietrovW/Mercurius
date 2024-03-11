446d1f92eaf406f76915d5eb46c0f586890f20c7
Piotr Walacik <piotr.walacik@icloud.com>
2024-03-11 22:06:37 +0100

    Update test (#6)

--- test/IntegrationTests/Repositories/ExceptionInfoRepositoryTest/GetExceptionInfoByID.cs
+++ test/IntegrationTests/Repositories/ExceptionInfoRepositoryTest/GetExceptionInfoByID.cs
@@ -1,4 +1,6 @@
-﻿using Domain.Common.Interfaces;
+﻿using Application.Entities;
+using Domain.Common.Interfaces;
+using Infrastructure.DAL;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,16 +8,41 @@ namespace IntegrationTests.Repositories.ExceptionInfoRepositoryTest;
 
 public class GetExceptionInfoByID
 {
-    //private readonly MercuriusContext _mercuriusContext;
-    //private readonly IExceptionInfoRepository _exceptionInfoRepository;
+    private readonly IDbContextFactory<MercuriusContext> _contextFactory;
+    private readonly IExceptionInfoRepository _exceptionInfoRepository;
 
-    //public GetExceptionInfoByID(ITestOutputHelper output)
-    //{
-    //    _output = output;
-    //    var dbOptions = new DbContextOptionsBuilder<MercuriusContext>()
-    //        .UseInMemoryDatabase(databaseName: "TestCatalog")
-    //        .Options;
-    //    _mercuriusContext = new MercuriusContext(dbOptions);
-    //    _exceptionInfoRepository = new EfRepository<Order>(_mercuriusContext);
-    //}
+    public GetExceptionInfoByID()
+    {
+        _contextFactory =new MigrationsContextFactory();
+        _exceptionInfoRepository = new ExceptionInfoRepository(dbContextFactory: _contextFactory);
+    }
+
+    [Test]
+    public async Task RemoveEmptyQuantities()
+    {
+        // Arrange
+        ExceptionInfoEntitie exceptionInfo = new ExceptionInfoEntitie() { 
+            InnerException =string.Empty,
+            Message = string.Empty, 
+            Source = string.Empty, 
+            StackTrace = string.Empty,
+            TargetSite = string.Empty,
+        };
+        // Act
+        await _exceptionInfoRepository.InsertExceptionInfoAsync(exceptionInfo: exceptionInfo);
+        await _exceptionInfoRepository.SaveAsync();
+        // Assert
+        Assert.AreEqual(0, 0);
+    }
+}
+public class MigrationsContextFactory : IDbContextFactory<MercuriusContext>
+{
+    public MercuriusContext CreateDbContext()
+    {
+        var dbOptions = new DbContextOptionsBuilder<MercuriusContext>()
+            .UseInMemoryDatabase(databaseName: $"MercuriusContext_{Guid.NewGuid()}")
+            .Options;
+
+        return new MercuriusContext(dbOptions);
+    }
 }
