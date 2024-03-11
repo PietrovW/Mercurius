8217cf42cf8a140f4b4cd71efd6cff4b1fc55033
Piotr Walacik <piotr.walacik@icloud.com>
2024-03-11 05:43:50 +0100

    testig update

---
+++ test/IntegrationTests/Repositories/ExceptionInfoRepositoryTest/GetExceptionInfoByID.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Common.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntegrationTests.Repositories.ExceptionInfoRepositoryTest;
+
+public class GetExceptionInfoByID
+{
+    private readonly MercuriusContext _mercuriusContext;
+    private readonly IExceptionInfoRepository _exceptionInfoRepository;
+
+    public GetExceptionInfoByID(ITestOutputHelper output)
+    {
+        _output = output;
+        var dbOptions = new DbContextOptionsBuilder<MercuriusContext>()
+            .UseInMemoryDatabase(databaseName: "TestCatalog")
+            .Options;
+        _mercuriusContext = new MercuriusContext(dbOptions);
+        _exceptionInfoRepository = new EfRepository<Order>(_mercuriusContext);
+    }
+}
