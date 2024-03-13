f73410fa7c674005d9f78c7cc5f8c77b8c5bca51
Piotr Walacik <piotr.walacik@icloud.com>
2024-03-13 06:41:25 +0100

    Update test (#6)

--- test/FunctionalTests/Api/ExceptionInfoEndpointTest.cs
+++ test/FunctionalTests/Api/ExceptionInfoEndpointTest.cs
@@ -1,4 +1,4 @@
-﻿using Api.Providers;
+﻿using Application.Entities;
 using Microsoft.AspNetCore.TestHost;
 using System.Net;
 
@@ -6,8 +6,9 @@ namespace FunctionalTests.Api;
 
 public class ExceptionInfoEndpointTest
 {
+
     [Fact]
-    public async Task CreatingExceptionInfoItemCommandShouldReturnIt()
+    public async Task GetAllExceptionInfoItemCommandShouldReturnStatusCodeOK()
     {
         // Arrange
         // Act
@@ -16,6 +17,46 @@ public class ExceptionInfoEndpointTest
         await application.InitializeAsync();
         var httpClient = application.Host.GetTestClient();
         var result = await httpClient.GetAsync("/api/exceptionInfo");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+    }
+    
+    [Fact]
+    public async Task CreatingExceptionInfoItemCommandShouldReturnIt()
+    {
+        // Arrange
+        var Id = Guid.NewGuid();
+        var exceptionInfo = new ExceptionInfoEntitie() {
+            Id = Guid.NewGuid(),
+            InnerException = $"InnerException_{Id} ",
+            Message = $"Message_{Id} ",
+            Source = $"Source_{Id} ",
+            StackTrace = $"StackTrace_{Id} ",
+            TargetSite = $"TargetSite_{Id} ",
+        };
+        // Act
+        AppFixture application = new AppFixture();
+
+        await application.InitializeAsync();
+        var httpClient = application.Host.GetTestClient();
+        var result = await httpClient.PostAsJsonAsync<ExceptionInfoEntitie>("/api/exceptionInfo",value: exceptionInfo);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.Created, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetByIdExceptionInfoItemCommandShouldReturnStatusCodeOK()
+    {
+        // Arrange
+        Guid Id = DataSeeder.Id;
+        // Act
+        AppFixture application = new AppFixture();
+
+        await application.InitializeAsync();
+        var httpClient = application.Host.GetTestClient();
+        var result = await httpClient.GetAsync($"/api/exceptionInfo/{Id}");
 
         // Assert
         Assert.Equal(HttpStatusCode.OK, result.StatusCode);
--- test/FunctionalTests/DataSeeder.cs
+++ test/FunctionalTests/DataSeeder.cs
@@ -5,12 +5,21 @@ namespace FunctionalTests;
 
 public class DataSeeder
 {
+    public static Guid Id = Guid.NewGuid();
     public static void SeedCountries(MercuriusContext context)
     {
         if (!context.ExceptionInfos.Any())
         {
             var exceptionInfos = new List<ExceptionInfoEntitie>();
-
+            exceptionInfos.Add(new ExceptionInfoEntitie
+            {
+                Id = Id,
+                InnerException = $"InnerException_{Guid.NewGuid()}",
+                Message = $"Message_{Guid.NewGuid()}",
+                Source = $"Source_{Guid.NewGuid()}",
+                StackTrace = $"StackTrace_{Guid.NewGuid()}",
+                TargetSite = $"TargetSite_{Guid.NewGuid()}",
+            });
             for (int i = 0; i < 100; i++)
             {
                 exceptionInfos.Add(new ExceptionInfoEntitie
@@ -26,6 +35,8 @@ public class DataSeeder
             
             context.ExceptionInfos.AddRange(exceptionInfos);
             context.SaveChanges();
-        }
+         
+           
+}
     }
 }
