3543c900ddf4d75fcf237e0d3d0a10799e86592d
Piotr Walacik <piotr.walacik@icloud.com>
2024-03-15 17:54:01 +0100

    Add file Authoriza

---
+++ src/Web/Web.Client/PersistentAuthenticationStateProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Web.Client;
+
+internal class PersistentAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private static readonly Task<AuthenticationState> defaultUnauthenticatedTask =
+        Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())));
+
+    private readonly Task<AuthenticationState> authenticationStateTask = defaultUnauthenticatedTask;
+
+    public PersistentAuthenticationStateProvider(PersistentComponentState state)
+    {
+        if (!state.TryTakeFromJson<UserInfo>(nameof(UserInfo), out var userInfo) || userInfo is null)
+        {
+            return;
+        }
+
+        Claim[] claims = [
+            new Claim(ClaimTypes.NameIdentifier, userInfo.UserId),
+            new Claim(ClaimTypes.Name, userInfo.Email),
+            new Claim(ClaimTypes.Email, userInfo.Email)];
+
+        authenticationStateTask = Task.FromResult(
+            new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims,
+                authenticationType: nameof(PersistentAuthenticationStateProvider)))));
+    }
+
+    public override Task<AuthenticationState> GetAuthenticationStateAsync() => authenticationStateTask;
+}
