0e5a6858d4aaf5bfcb69b7fec3618d6155e37769
Piotr Walacik <piotr.walacik@icloud.com>
2024-03-28 16:38:39 +0100

    Configuration in the project Versioning API #11

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -13,11 +13,21 @@ using Domain.Events;
 using Api.Extensions;
 using Api.Options;
 using Api.Authentication;
+using Asp.Versioning;
 
 [assembly: InternalsVisibleTo("FunctionalTests")]
 
 var builder = WebApplication.CreateSlimBuilder(args);
-
+var apiVersioningBuilder = builder.Services.AddApiVersioning(o =>
+{
+    o.AssumeDefaultVersionWhenUnspecified = true;
+    o.DefaultApiVersion = new ApiVersion(2, 0);
+    o.ReportApiVersions = true;
+    o.ApiVersionReader = ApiVersionReader.Combine(
+        new QueryStringApiVersionReader("api-version"),
+        new HeaderApiVersionReader("X-Version"),
+        new MediaTypeApiVersionReader("ver"));
+});
 builder.Configuration.AddEnvironmentVariables(prefix: "Mercurius_");
 ConfigurationManager configuration = builder.Configuration;
 builder.Services.Configure<MercuriusOptions>(
@@ -37,6 +47,12 @@ builder.Host.UseWolverine(options =>
 builder.Services.ConfigureInfrastructureServices();
 
 var app = builder.Build();
+var apiVersionSet = app.NewApiVersionSet()
+    .HasDeprecatedApiVersion(new ApiVersion(1, 0))
+    .HasApiVersion(new ApiVersion(2, 0))
+    .HasApiVersion(new ApiVersion(3, 0))
+    .ReportApiVersions()
+    .Build();
 app.UseHttpsRedirection();
 if (app.Environment.IsDevelopment())
 {
@@ -54,7 +70,8 @@ using (var scope = app.Services.CreateScope())
 app.MapPost("/api/exceptionInfo", async (CreateExceptionInfoItemCommand body, IMessageBus bus) =>
     await bus.InvokeAsync<ExceptionInfoCreatedEvent>(body) is ExceptionInfoCreatedEvent exceptionInfos ?
      Results.Created($"/exceptionInfoItems/{exceptionInfos.Id}", body) : Results.BadRequest()
-).WithOpenApi().RequireAuthorization(RolesConstants.RolaAdd);
+).WithOpenApi().RequireAuthorization(RolesConstants.RolaAdd).WithApiVersionSet(apiVersionSet)
+    .MapToApiVersion(new ApiVersion(1, 0));
 app.MapGet("/api/exceptionInfo/", async (IMessageBus bus) => await bus.InvokeAsync<IEnumerable<ExceptionInfoEntitie>>(new GetAllExceptionInfoQuerie()) is IEnumerable<ExceptionInfoEntitie> exceptionInfos
          ? Results.Ok(exceptionInfos)
          : Results.NotFound())
@@ -63,13 +80,15 @@ app.MapGet("/api/exceptionInfo/", async (IMessageBus bus) => await bus.InvokeAsy
    .WithOpenApi(operation => new(operation)
    {
        OperationId = "GetExceptionInfo"
-   }).RequireAuthorization(RolesConstants.RolaAdd);
+   }).RequireAuthorization(RolesConstants.RolaAdd).WithApiVersionSet(apiVersionSet)
+    .MapToApiVersion(new ApiVersion(1, 0));
 
 app.MapGet("/api/exceptionInfo/{id}", async (Guid id, IMessageBus bus) => await bus.InvokeAsync<ExceptionInfoEntitie>(new GetExceptionInfoByIdQuerie(Id: id)) is ExceptionInfoEntitie item
             ? Results.Ok(item)
             : Results.NotFound())
      .Produces<ExceptionInfoEntitie>(StatusCodes.Status200OK)
-   .Produces(StatusCodes.Status404NotFound).RequireAuthorization(RolesConstants.RolaAdd);
+   .Produces(StatusCodes.Status404NotFound).RequireAuthorization(RolesConstants.RolaAdd).WithApiVersionSet(apiVersionSet)
+    .MapToApiVersion(new ApiVersion(1, 0));
 
 app.UseHttpsRedirection();
 return await app.RunOaktonCommands(args);
