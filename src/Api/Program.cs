a9e0f2e3f57bd07ffb47a0f9813ec40a17685320
Piotr Walacik <piotr.walacik@icloud.com>
2024-04-02 22:08:31 +0200

    update

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -14,20 +14,12 @@ using Api.Extensions;
 using Api.Options;
 using Api.Authentication;
 using Asp.Versioning;
+using Asp.Versioning.Builder;
 
 [assembly: InternalsVisibleTo("FunctionalTests")]
 
 var builder = WebApplication.CreateSlimBuilder(args);
-//var apiVersioningBuilder = builder.Services.AddApiVersioning(o =>
-//{
-//    o.AssumeDefaultVersionWhenUnspecified = true;
-//    o.DefaultApiVersion = new ApiVersion(2, 0);
-//    o.ReportApiVersions = true;
-//    o.ApiVersionReader = ApiVersionReader.Combine(
-//        new QueryStringApiVersionReader("api-version"),
-//        new HeaderApiVersionReader("X-Version"),
-//        new MediaTypeApiVersionReader("ver"));
-//});
+
 builder.Configuration.AddEnvironmentVariables(prefix: "Mercurius_");
 ConfigurationManager configuration = builder.Configuration;
 builder.Services.Configure<MercuriusOptions>(
@@ -47,11 +39,13 @@ builder.Host.UseWolverine(options =>
 builder.Services.ConfigureInfrastructureServices();
 
 var app = builder.Build();
-//var apiVersionSet = app.NewApiVersionSet()
-//    .HasDeprecatedApiVersion(new ApiVersion(1, 0))
-//    .HasApiVersion(new ApiVersion(2, 0))
-//    .ReportApiVersions()
-//    .Build();
+ApiVersionSet apiVersionSet = app.NewApiVersionSet()
+    .HasApiVersion(new ApiVersion(1))
+    .HasApiVersion(new ApiVersion(2))
+    .ReportApiVersions()
+    .Build();
+
+
 app.UseHttpsRedirection();
 if (app.Environment.IsDevelopment())
 {
@@ -67,27 +61,26 @@ using (var scope = app.Services.CreateScope())
     await MercuriusContext.InitializeAsync(datebase);
 }
 app.MapPost("/api/exceptionInfo", async (CreateExceptionInfoItemCommand body, IMessageBus bus) =>
-    await bus.InvokeAsync<ExceptionInfoCreatedEvent>(body) is ExceptionInfoCreatedEvent exceptionInfos ?
-     Results.Created($"/exceptionInfoItems/{exceptionInfos.Id}", body) : Results.BadRequest()
-).WithOpenApi().RequireAuthorization(RolesConstants.RolaAdd);//.WithApiVersionSet(apiVersionSet)
-  //  .MapToApiVersion(new ApiVersion(1, 0));
+        await bus.InvokeAsync<ExceptionInfoCreatedEvent>(body) is ExceptionInfoCreatedEvent exceptionInfos ?
+         Results.Created($"/exceptionInfoItems/{exceptionInfos.Id}", body) : Results.BadRequest()
+    ).WithOpenApi().RequireAuthorization(RolesConstants.RolaAdd);
 app.MapGet("/api/exceptionInfo/", async (IMessageBus bus) => await bus.InvokeAsync<IEnumerable<ExceptionInfoEntitie>>(new GetAllExceptionInfoQuerie()) is IEnumerable<ExceptionInfoEntitie> exceptionInfos
-         ? Results.Ok(exceptionInfos)
-         : Results.NotFound())
-     .Produces<ExceptionInfoEntitie>(StatusCodes.Status200OK)
-   .Produces(StatusCodes.Status404NotFound)
-   .WithOpenApi(operation => new(operation)
-   {
-       OperationId = "GetExceptionInfo"
-   }).RequireAuthorization(RolesConstants.RolaAdd);//.WithApiVersionSet(apiVersionSet)
-                                                   // .MapToApiVersion(new ApiVersion(1, 0));
+             ? Results.Ok(exceptionInfos)
+             : Results.NotFound())
+         .Produces<ExceptionInfoEntitie>(StatusCodes.Status200OK)
+       .Produces(StatusCodes.Status404NotFound)
+       .WithOpenApi(operation => new(operation)
+       {
+           OperationId = "GetExceptionInfo"
+       }).RequireAuthorization(RolesConstants.RolaAdd);
 
 app.MapGet("/api/exceptionInfo/{id}", async (Guid id, IMessageBus bus) => await bus.InvokeAsync<ExceptionInfoEntitie>(new GetExceptionInfoByIdQuerie(Id: id)) is ExceptionInfoEntitie item
-            ? Results.Ok(item)
-            : Results.NotFound())
-     .Produces<ExceptionInfoEntitie>(StatusCodes.Status200OK)
-   .Produces(StatusCodes.Status404NotFound).RequireAuthorization(RolesConstants.RolaAdd);//.WithApiVersionSet(apiVersionSet)
-   // .MapToApiVersion(new ApiVersion(1, 0));
+                ? Results.Ok(item)
+                : Results.NotFound())
+         .Produces<ExceptionInfoEntitie>(StatusCodes.Status200OK)
+       .Produces(StatusCodes.Status404NotFound).RequireAuthorization(RolesConstants.RolaAdd);
 
 app.UseHttpsRedirection();
+
+
 return await app.RunOaktonCommands(args);
